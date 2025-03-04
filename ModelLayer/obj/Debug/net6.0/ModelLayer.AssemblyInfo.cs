d230dbb55e8a4e5348865d48c5e460d5b8ac31ec
Jyoti <thakurjyoti1430@gmail.com>
2025-03-04 13:30:40 +0530

    [Jyoti] add : adding UC2 code into branch

--- ModelLayer/obj/Debug/net6.0/ModelLayer.AssemblyInfo.cs
+++ ModelLayer/obj/Debug/net6.0/ModelLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2f83448f32b1eac9605487e299a14078a4f7692")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2809068b24e34ac34339857ce30beebdd5f7922b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
